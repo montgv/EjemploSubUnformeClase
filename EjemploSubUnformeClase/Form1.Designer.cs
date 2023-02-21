05b2e851ace86f4f536affbbf949f3c52fe8ecfd
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2023-02-21 14:35:25 +0100

    Agregar archivos de proyecto.

---
+++ EjemploSubUnformeClase/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace EjemploSubUnformeClase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dataSet1 = new EjemploSubUnformeClase.DataSet1();
+            this.cochesVendidosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cochesVendidosTableAdapter = new EjemploSubUnformeClase.DataSet1TableAdapters.CochesVendidosTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cochesVendidosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.cochesVendidosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "EjemploSubUnformeClase.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 22);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(762, 397);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cochesVendidosBindingSource
+            // 
+            this.cochesVendidosBindingSource.DataMember = "CochesVendidos";
+            this.cochesVendidosBindingSource.DataSource = this.dataSet1;
+            // 
+            // cochesVendidosTableAdapter
+            // 
+            this.cochesVendidosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cochesVendidosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource cochesVendidosBindingSource;
+        private DataSet1TableAdapters.CochesVendidosTableAdapter cochesVendidosTableAdapter;
+    }
+}
+
---
+++ EjemploSubUnformeClase/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploSubUnformeClase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dataSet1.CochesVendidos' Puede moverla o quitarla según sea necesario.
+            this.cochesVendidosTableAdapter.Fill(this.dataSet1.CochesVendidos);
+
+            this.reportViewer1.RefreshReport();
+
+            this.reportViewer1.LocalReport.SubreportProcessing += LocalReport_SubreportProcessing;
+        }
+
+        private void LocalReport_SubreportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            //Guardo la idVendedor
+            var codeID = int.Parse(e.Parameters["idCliente"].Values.First());
+            //Creo un dataset y lo relleno
+            DataSet1.CochesVendidosDataTable prueba = new DataSet1.CochesVendidosDataTable();
+            this.cochesVendidosTableAdapter.FillBy(prueba, codeID.ToString());
+
+            e.DataSources.Add(new ReportDataSource("DataSet1", (DataTable)prueba));
+        }
+    }
+}
